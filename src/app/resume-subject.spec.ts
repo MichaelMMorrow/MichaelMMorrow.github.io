2aabc49fca6c30da33b0ed980b4d5d4ed63d8994
Mitchell Morrow <mitchmmorrow@gmail.com>
2021-09-05 13:41:10 -0400

    update

--- src/app/resume-subject.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ResumeSubject } from './resume-subject';
-
-describe('ResumeSubject', () => {
-  it('should create an instance', () => {
-    expect(new ResumeSubject()).toBeTruthy();
-  });
-});
